66cd5a1e340d14d724781d05d2fb38aa9de764b7
Rinkit Adhana <therinkit@gmail.com>
2024-09-18 17:09:21 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 // require("dotenv").config({ path: "./env" })
 
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+import dotenv from "dotenv";
 
 dotenv.config({
   path: "./env",
-})
+});
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error :", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Listening on ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error", err);
+  });
 // const app = express()
 //import express from "express"
 // ;(async () => {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asyncHadler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHadler };
